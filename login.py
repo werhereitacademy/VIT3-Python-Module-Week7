6bbc8e2ae507874d53688bb6d37d6e208b84af21
Fatih <121514395+buyukakcali@users.noreply.github.com>
2024-03-12 02:20:00 +0100

    main.py and login.py finished

--- login.py
+++ login.py
@@ -1,12 +1,22 @@
-from PyQt6.QtWidgets import*
-from login_bak import Ui_MainWindow
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6 import QtCore
+import gspread
+
+
+from login_ui import Ui_MainWindow
 from user_admin_preference import UserAdminPreferencePage
 from user_preference import UserPreferencePage
-from PyQt6.QtWidgets import QMainWindow
-from PyQt6 import uic, QtCore
+
+credentials = 'key.json'
+gc = gspread.service_account(filename=credentials)
+spreadsheet_users = gc.open('Kullanicilar')
+worksheet_users = spreadsheet_users.get_worksheet(0)
+users = worksheet_users.get_all_values()
+users.pop(0)
+
 
 class LoginPage(QMainWindow):
-    def __init__(self) :
+    def __init__(self):
         super().__init__()
         self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
@@ -14,43 +24,29 @@ class LoginPage(QMainWindow):
         self.loginform.setupUi(self)
         self.useradminwindow_open = UserAdminPreferencePage()
         self.userprewindow_open = UserPreferencePage()
-        self.loginform.pushButton_log_login.clicked.connect(self.log_in)
-        self.loginform.pushButton_log_exit.clicked.connect(self.log_exit)
-        
 
+        # Herhangi bir anda Enter tusuna basinca yetki kontrolu yapmak icin kodlar
+        self.loginform.lineEdit_log_password.returnPressed.connect(self.app_login)
+        self.loginform.lineEdit_log_password.returnPressed.connect(self.app_login)
 
-    def log_exit(self):
+        # 'pushButton_log_login' butonuna tiklandiginda yetki kontrolu yapmak icin kodlar
+        self.loginform.pushButton_log_login.clicked.connect(self.app_login)
+        self.loginform.pushButton_log_exit.clicked.connect(self.app_exit)
+
+    def app_login(self):
+        username = self.loginform.lineEdit_log_username.text()
+        password = self.loginform.lineEdit_log_password.text()
+        for user in users:
+            if username == user[0] and password == user[1] and user[2] == 'admin':
+                self.close()
+                self.useradminwindow_open.show()
+            elif username == user[0] and password == user[1] and user[2] == 'user':
+                self.close()
+                self.userprewindow_open.show()
+            else:
+                self.loginform.label_log_fail.setText("\tYour email or password is incorrect.")
+                self.loginform.lineEdit_log_username.setText("")
+                self.loginform.lineEdit_log_password.setText("")
+
+    def app_exit(self):
         self.close()
-
-    def log_in(self):
-        username = self.loginform.lineEdit_log_username.text()   
-        password = self.loginform.lineEdit_log_password.text()  
-        if username == "a" and password == "1":
-            self.close()
-            self.useradminwindow_open.show()
-        elif username == "u" and password == "2":
-            self.close()
-            self.userprewindow_open.show() 
-        else:
-            self.loginform.label_log_fail.setText("\tYour email or password is incorrect.")
-            self.loginform.lineEdit_log_username.setText("")
-            self.loginform.lineEdit_log_password.setText("")
-            
-
-
-            
-        
-    
-
-                   
-                
-
-
-
-            
-        
-        
-
-            
-        
-                
--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 from PyQt6.QtWidgets import QApplication
 from login import LoginPage
 
-import gspread
 
-credentials = 'key.json'
-gc = gspread.service_account(filename=credentials)
-spreadsheet = gc.open('Basvurular')
-worksheet = spreadsheet.get_worksheet(0)
-print(worksheet.get_all_values())
-
-app = QApplication([])
-window = LoginPage()
-window.show()
-app.exec()
-
-
-    
+if __name__ == '__main__':
+    app = QApplication([])
+    window = LoginPage()
+    window.show()
+    app.exec()
